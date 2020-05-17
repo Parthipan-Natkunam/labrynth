6d5e86ba25542915a8015f95d95eab6e89c42db7
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2020-05-17 21:25:48 +0530

    initial commit with basic fiber setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Parthipan-Natkunam/labrynth
+
+go 1.14
+
+require github.com/gofiber/fiber v1.9.6
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) {
+		ctx.Send("Fiber Route Inited")
+	})
+
+	app.Listen(3000)
+}
